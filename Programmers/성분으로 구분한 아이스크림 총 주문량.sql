3fb278a2a0283eac12a9da338223e5bbd35251a3
RaemorrisV <88121150+RaemorrisV@users.noreply.github.com>
2023-06-01 12:37:37 +0900

    성분으로 구분한 아이스크림 총 주문량

---
+++ Programmers/성분으로 구분한 아이스크림 총 주문량.sql
@@ -0,0 +1,5 @@
+SELECT i.INGREDIENT_TYPE, sum(f.TOTAL_ORDER) as 'TOTAL_ORDER'
+    from FIRST_HALF f join ICECREAM_INFO i
+    on f.flavor = i.flavor
+    group by i.INGREDIENT_TYPE
+    order by sum(f.TOTAL_ORDER);
